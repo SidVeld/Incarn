3b37302ecd9bfbe5ebf6c06410ce8683facd9906
SidVeld Von Disco <sidveld@gmail.com>
2021-12-09 19:06:36 +0300

    Rework roll command

--- incarn/exts/games/dice_roll.py
+++ incarn/exts/games/dice_roll.py
@@ -12,43 +12,63 @@ from incarn.bot import IncarnBot
 class DiceRoll(commands.Cog):
 
     @commands.command(name="roll", aliases=["r"])
-    async def roll_command(self, ctx: Context, dice: str, mod: int = 0):
+    async def roll_command(self, ctx: Context, expression: str):
         """
         Classic dice roller.
 
         For `dice` type `{x}d{y}`.
         Where `x` - count of dices and `y` - edges of dices.
 
-        You can also use the `mod` parameter. It adds its own value to the result of the throw.
-        If you get 5, and the `mod` parameter is 2, your result will change according to the formula `roll + mod`
-
         Examples:
         `1d10` -> Rolls 1 dice with 10 edges. Returns a number from 1 to 10.
         `2d15` -> Rolls 2 dices with 15 edges. Returns two numbers from 1 to 15.
-        `1d5 1` -> Rolls 1 dice with 5 edges. Returns a number from 1 to 5, to which 1 is added.
-        `1d5 -1` -> Rolls 1 dice with 5 edges. Returns a number from 1 to 5, from which 1 will be subtracted.
+        `1d5+1` -> Rolls 1 dice with 5 edges. Returns a number from 1 to 5, to which 1 is added.
+        `1d5-1` -> Rolls 1 dice with 5 edges. Returns a number from 1 to 5, from which 1 will be subtracted.
         """
-        dice_splitted = re.split("d", dice)
-        dice_count = int(dice_splitted[0])
-        dice_edge = int(dice_splitted[1])
+
+        if len(expression) < 3:
+            await ctx.send("Too short!")
+            return
+
+        expression_split = expression.split("d")
+
+        count = int(expression_split[0])
+
+        sides_and_mod = re.split("(\+|\-|\*|//|/)", expression_split[1])  # noqa W605
+
+        sides = int(sides_and_mod[0])
+
+        mod_str = " ".join(sides_and_mod[1:])
 
         rolls = []
-        rolls_modded = []
 
-        while dice_count > 0:
-            dice_result = random.randint(1, dice_edge)
-            rolls.append(dice_result)
-            rolls_modded.append(dice_result + mod)
-            dice_count -= 1
+        while count > 0:
+            roll = random.randint(1, sides)
+            rolls.append(roll)
+            count -= 1
 
-        result = ", ".join(str(roll) for roll in rolls)
+        rolls_mod = []
 
-        if mod:
-            result += f"\n**Modded** ({mod})\n" + ", ".join(str(roll) for roll in rolls_modded)
+        for roll in rolls:
+            expr = str(roll) + mod_str
+            rolls_mod.append(int(eval(expr)))
+
+        message = "Here you go\n"
+
+        for roll, roll_modded in zip(rolls, rolls_mod):
+            roll_string = f"{roll} [ {mod_str} ] ➞ **{roll_modded}**"
+            message += roll_string + "\n"
+
+        roll_summ = 0
+
+        for roll in rolls_mod:
+            roll_summ += roll
+
+        message += f"\n**Total**: {roll_summ}\n"
 
         embed = Embed()
         embed.set_author(name="Roll result")
-        embed.description = result
+        embed.description = message
 
         await ctx.send(embed=embed)
 
