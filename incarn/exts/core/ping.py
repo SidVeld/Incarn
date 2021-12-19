7b48fe84447ea72f82c8a50713750f4051b754ba
SidVeld Von Disco <sidveld@gmail.com>
2021-12-20 00:02:39 +0300

    Add uptime command

--- incarn/exts/core/ping.py
+++ incarn/exts/core/ping.py
@@ -1,5 +1,9 @@
+import arrow
+from dateutil.relativedelta import relativedelta
+
 from discord import Embed
 from discord.ext import commands
+from discord.ext.commands import Context
 
 from incarn.bot import IncarnBot
 from incarn.constants import Colours
@@ -14,13 +18,13 @@ ROUND_LATENCY = 3
 
 
 class Latency(commands.Cog):
-    """Getting the latency between the bot and websites."""
+    """Getting the latency and uptime of the bot."""
 
     def __init__(self, bot: IncarnBot) -> None:
         self.bot = bot
 
     @commands.command(name="ping")
-    async def ping(self, ctx: commands.Context) -> None:
+    async def ping(self, ctx: Context) -> None:
         """Ping the bot to see its latency and state."""
         embed = Embed(
             title=":ping_pong: Pong!",
@@ -29,6 +33,19 @@ class Latency(commands.Cog):
         )
 
         await ctx.send(embed=embed)
+
+    @commands.command(name="uptime")
+    async def uptime(self, ctx: Context) -> None:
+        """Get the current uptime of the bot."""
+        difference = relativedelta(self.bot.start_time - arrow.utcnow())
+        uptime_string = self.bot.start_time.shift(
+          seconds=difference.seconds,
+          minutes=difference.minutes,
+          hours=difference.hours,
+          days=difference.days,
+        ).humanize()
+
+        await ctx.send(f"I started working {uptime_string}!")
 
 
 def setup(bot: IncarnBot) -> None:
