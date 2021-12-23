1d1fa549d09e23b96f549042c6e832571a20e203
SidVeld Von Disco <sidveld@gmail.com>
2021-12-23 22:31:37 +0300

    Convert temperature converters to subcommands

--- incarn/exts/utils/converters.py
+++ incarn/exts/utils/converters.py
@@ -3,63 +3,42 @@ from discord.ext.commands import Cog, Context
 
 from incarn.bot import IncarnBot
 
+ONE_KELVIN = 273.15
+
 
 class Converters(Cog):
     """Usefull converters."""
 
-    def __init__(self, bot: IncarnBot):
-        self.bot = bot
+    @commands.group(name="convert", aliases=("cnvrt", "cnv"))
+    async def convert_command(self, ctx: Context):
+        """Simple converters that converts something to something."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(str(ctx.command))
 
-    @commands.command()
-    async def c2f(self, ctx: Context, amount: float = 0) -> None:
+    @convert_command.command(name="c2f")
+    async def convert_c2f(self, ctx: Context, amount: float = 0):
         """Conversion of Celsius to Fahrenheit."""
         result = round((amount * 9 / 5) + 32, 2)
         await ctx.send(f"{amount}°C 🠖 {result}°F")
 
-    @commands.command()
-    async def f2c(self, ctx: Context, amount: float = 0) -> None:
+    @convert_command.command(name="f2c")
+    async def convert_f2c(self, ctx: Context, amount: float = 0):
         """Convert Fahrenheit to Celsius."""
-        result = round((amount - 32) / 1.8, 2)
+        result = round((amount - 32) * 9 / 5, 2)
         await ctx.send(f"{amount}°F 🠖 {result}°C")
 
-    @commands.command()
-    async def c2k(self, ctx: Context, amount: float = 0) -> None:
+    @convert_command.command(name="c2k")
+    async def convert_c2k(self, ctx: Context, amount: float = 0):
         """Converts Celsius to Kelvin."""
-        result = round(amount + 273.15, 2)
+        result = round(amount + ONE_KELVIN, 2)
         await ctx.send(f"{amount}°C 🠖 {result}K")
 
-    @commands.command()
-    async def k2c(self, ctx: Context, amount: float = 0) -> None:
+    @convert_command.command(name="k2c")
+    async def convert_k2c(self, ctx: Context, amount: float = 0):
         """Converts Kelvins to Celsius."""
-        result = round(amount - 273.15, 2)
+        result = round(amount - ONE_KELVIN, 2)
         await ctx.send(f"{amount}K 🠖 {result}°C")
-
-    @commands.command()
-    async def hex2rgb(self, ctx: Context, hex_code: str = "#FFFFFF") -> None:
-        """Converts HEX to RGB."""
-        if hex_code.startswith("#"):
-            hex_code = hex_code.replace("#", "")
-        rgb_code = tuple(int(hex_code[i:i + 2], 16) for i in (0, 2, 4))
-        await ctx.send(f"HEX: #{hex_code} 🠖 RGB: {rgb_code}")
-
-    @commands.command()
-    async def rgb2hex(self, ctx: Context, r=255, g=255, b=255) -> None:
-        """Converts RGB to HEX."""
-        hex_code = "#%02x%02x%02x" % (r, g, b)
-        await ctx.send(f"RGB: ({r}, {g}, {b}) 🠖 HEX: {hex_code}")
-
-    # @commands.command()
-    # async def color(self, ctx: Context, hex_code: str) -> None:
-    #     """Generates colored embed."""
-
-    #     if hex_code.startswith("#"):
-    #         hex_code = hex_code.replace("#", "")
-
-    #     embed = discord.Embed(description=f"HEX: `#{hex_code}`", color=int(hex_code, 16))
-    #     embed.set_image(url=f"http://placehold.it/240x120.png/{hex_code}/{hex_code}")
-
-    #     await ctx.send(embed=embed)
 
 
 def setup(bot: IncarnBot):
-    bot.add_cog(Converters(bot))
+    bot.add_cog(Converters())
