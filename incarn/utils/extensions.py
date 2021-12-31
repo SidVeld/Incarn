0fd3ff17591cb7b54d77807de40c1f018dca5b10
SidVeld Von Disco <sidveld@gmail.com>
2022-01-01 01:36:28 +0300

    Remove invoke_help_command

--- incarn/utils/extensions.py
+++ incarn/utils/extensions.py
@@ -1,10 +1,7 @@
 import importlib
 import inspect
 import pkgutil
-from collections.abc import Iterator
-from typing import NoReturn
-
-from discord.ext.commands import Context
+from typing import Iterator, NoReturn
 
 from incarn import exts
 
@@ -31,13 +28,5 @@ def walk_extensions() -> Iterator[str]:
 
         yield module.name
 
-
-async def invoke_help_command(ctx: Context) -> None:
-    """Invoke the help command or default help command if help extensions is not loaded."""
-    if "bot.exts.core.help" in ctx.bot.extensions:
-        help_command = ctx.bot.get_command("help")
-        await ctx.invoke(help_command, ctx.command.qualified_name)
-        return
-    await ctx.send_help(ctx.command)
 
 EXTENSIONS = frozenset(walk_extensions())
