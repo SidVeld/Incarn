e8c502564ee57fe78c1dd1315d7b2d6d4b69dae8
SidVeld Von Disco <sidveld@gmail.com>
2022-01-01 16:13:36 +0300

    Convert covid command to group

--- incarn/exts/info/covid.py
+++ incarn/exts/info/covid.py
@@ -1,5 +1,5 @@
 from discord import Embed
-from discord.ext.commands import Cog, Context, command
+from discord.ext.commands import Cog, Context, group
 
 from incarn.bot import IncarnBot
 from incarn.log import get_logger
@@ -13,16 +13,28 @@ class Covid19(Cog):
     def __init__(self, bot: IncarnBot):
         self.bot = bot
 
-    @command(name="covid19", aliases=("covid", "cvd19"))
-    async def covid19(self, ctx: Context, *, country: str):
-        """Covid-19 Statistics for any countries."""
-        response = await self.bot.http_session.get(f"https://disease.sh/v3/covid-19/countries/{country.lower()}")
+    @group(name="covid19", aliases=("covid", "covid2019"))
+    async def covid(self, ctx: Context) -> None:
+        """Commands for recieving information about covid-19 in world."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @covid.command(name="now")
+    async def covid_now(self, ctx: Context, *, country: str = None):
+        """Covid-19 Statistics for country."""
+
+        all_alieases = ("all", "worldwide", "ww")
+
+        if country in all_alieases or country is None:
+            response = await self.bot.http_session.get("https://disease.sh/v3/covid-19/all")
+        else:
+            response = await self.bot.http_session.get(f"https://disease.sh/v3/covid-19/countries/{country.lower()}")
 
         if response.status == 200:
             response_json = await response.json(encoding="UTF-8")
         else:
             log.debug(f"Failed to get covid data. Status code {response.status}")
-            await ctx.send(f"We got {response.status}")
+            await ctx.send(f"We got {response.status}.")
             return
 
         covid_information = [
@@ -42,11 +54,17 @@ class Covid19(Cog):
 
         embed.title = ":microbe: Covid-19"
 
-        embed.description = (
-            f"Selected country: :flag_{response_json['countryInfo']['iso2'].lower()}: **{country.capitalize()}**\n"
-            f"Updated <t:{int(response_json['updated'] / 1000)}:R>"
-            "\n────────────────────────"
-        )
+        if country in all_alieases or country is None:
+            embed.description = (
+                f"Updated <t:{int(response_json['updated'] / 1000)}:R>"
+                "\n────────────────────────"
+            )
+        else:
+            embed.description = (
+                f"Selected country: :flag_{response_json['countryInfo']['iso2'].lower()}: **{country.capitalize()}**\n"
+                f"Updated <t:{int(response_json['updated'] / 1000)}:R>"
+                "\n────────────────────────"
+            )
 
         for name, value in covid_information:
             embed.add_field(
