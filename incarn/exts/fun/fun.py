a2ec4f8e0ea09a0b4719824d37d5fc50e9f7fee4
SidVeld Von Disco <sidveld@gmail.com>
2022-01-14 21:34:03 +0300

    Move KanyeQuote command to Fun extension

--- incarn/exts/fun/fun.py
+++ incarn/exts/fun/fun.py
@@ -157,6 +157,29 @@ class Fun(Cog):
 
         await ctx.send(embed=embed)
 
+    @command(name="kanye_quote", aliases=("kq", "kwq", "kanye", "kanyewest"))
+    async def kanye_quote(self, ctx: Context) -> None:
+        """
+        Sends random Kanye West quote.
+
+        Thank you, Kanye, very cool.
+        """
+        async with self.bot.http_session.get("https://api.kanye.rest") as response:
+
+            if response.status != 200:
+                await ctx.send(f"Something goes wrong. Code: {response.status}")
+                return
+
+            response = await response.json()
+            quote = response["quote"]
+
+            embed = Embed(
+                title="Kanye West Quote",
+                description=quote
+            )
+
+            await ctx.send(embed=embed)
+
 
 def setup(bot: IncarnBot) -> None:
     """Load the Fun cog."""
