01aa5db5563473a8e0fcff60797e5fe5d8da957e
SidVeld Von Disco <sidveld@gmail.com>
2022-01-17 22:40:10 +0300

    Move Events to Core folder

---
+++ incarn/exts/core/events.py
@@ -0,0 +1,137 @@
+import arrow
+import yaml
+from arrow import Arrow
+from pathlib import Path
+from typing import Optional
+
+from discord import Embed
+from discord.channel import TextChannel
+from discord.ext import tasks
+from discord.ext.commands import Cog
+
+from incarn.bot import IncarnBot
+from incarn.constants import Channels
+from incarn.log import get_logger
+from incarn.utils import scheduling
+
+
+log = get_logger(__name__)
+
+num_to_month = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December",
+}
+
+EVENTS_DIR = Path("incarn/resources/events")
+
+
+class Events(Cog):
+    """
+    This module allows the bot to track holidays known to it
+    and remind everyone about them on the server.
+    """
+
+    def __init__(self, bot: IncarnBot) -> None:
+        self.bot = bot
+        self.scheduler = scheduling.Scheduler("EventsScheduler")
+        self._plan_fetch_task = scheduling.create_task(self._plan_fetch(), event_loop=self.bot.loop)
+
+    def cog_unload(self) -> None:
+        self.scheduler.cancel_all()
+        self.check_day.stop()
+
+    async def _plan_fetch(self):
+        today = arrow.now()
+        next_day = today.shift(days=1)
+        next_day_midnight = next_day.replace(hour=0, minute=1, second=0, microsecond=0)
+        self.scheduler.schedule_at(next_day_midnight, 1, self._start_checking())
+
+    async def _start_checking(self):
+        self.check_day.start()
+
+    @tasks.loop(hours=24)
+    async def check_day(self, ctx):
+        """Checks day for special events."""
+        today = arrow.now()
+        await self.fetch_birthdays(today)
+        await self.fetch_holidays(today)
+
+    async def fetch_birthdays(self, today: Arrow) -> None:
+        """Fetches birthdays and sending embed if today is someone's burthday."""
+        today_str = today.format("DD-MM")
+        log.debug(f"Fetching birthdays with {today_str}")
+
+        b_file = EVENTS_DIR / "birthdays.yml"
+        birthdays: dict = yaml.load(b_file.open(encoding="UTF-8"), Loader=yaml.FullLoader)
+
+        for birthday in birthdays.values():
+
+            if birthday["date"] != today_str:
+                continue
+
+            name: str = birthday["name"]
+            text: str = birthday["text"]
+            date: str = birthday["date"]
+            year: Optional[int] = birthday["year"]
+
+            log.trace(f"Found birthday: {name}. Builing embed.")
+
+            embed = Embed(
+                title=f":birthday: Today is the birthday of: {name}",
+                description=text,
+                colour=0xec586d
+            )
+
+            embed.add_field(name="Date of birth", value=date)
+
+            if year is not None:
+                embed.add_field(name="Age as of today", value=int(today.format("YYYY")) - year)
+
+            channel: TextChannel = self.bot.get_channel(Channels.announcements)
+            await channel.send(embed=embed)
+
+    async def fetch_holidays(self, today: Arrow) -> None:
+        """Fetches holidays and sending embed if today is a special holiday."""
+        today_str = today.format("DD-MM")
+        log.debug(f"Fetching birthdays with {today_str}")
+
+        h_file = EVENTS_DIR / "holidays.yml"
+        holidays: dict = yaml.load(h_file.open(encoding="UTF-8"), Loader=yaml.FullLoader)
+
+        for holiday in holidays.values():
+
+            if holiday["date"] != today_str:
+                continue
+
+            name: str = holiday["name"]
+            desc: str = holiday["desc"]
+            color: str = holiday["color"]
+            emoji: str = holiday["emoji"]
+
+            log.trace(f"Found holiday: {name}. Builing embed.")
+
+            embed = Embed(
+                title=f":{emoji}: {name}",
+                description=desc,
+                colour=color
+            )
+
+            channel: TextChannel = self.bot.get_channel(Channels.announcements)
+
+            await channel.send(embed=embed)
+
+        log.debug("Finished fetching holidays")
+
+
+def setup(bot: IncarnBot) -> None:
+    bot.add_cog(Events(bot))
