aa5bacf5a87a9e50a58e1b82b71b011beb9b2f44
SidVeld Von Disco <sidveld@gmail.com>
2022-01-28 17:40:02 +0300

    Update WOD roll commands

--- incarn/exts/games/world_of_darkness.py
+++ incarn/exts/games/world_of_darkness.py
@@ -1,24 +1,24 @@
-import discord
 import random
 import textwrap
 
+from discord import Embed
 from discord.ext import commands
+from discord.ext.commands import Cog, group, Context
 
 from incarn.bot import IncarnBot
 
 
 class RollColors:
-    success_crit = 0x78e08f
-    success = 0xb8e994
-    failure = 0xe55039
-    failure_crit = 0xeb2f06
+    success = 0x74BDCB
+    failure = 0xEFE7BC
+    failure_crit = 0xFFA384
 
 
-class WorldOfDarkness(commands.Cog):
+class WorldOfDarkness(Cog):
     """Cog for World Of Darkness commands."""
 
-    @commands.group(name="world_of_darkness", aliases=["wod"])
-    async def world_of_darkness(self, ctx: commands.Context):
+    @group(name="world_of_darkness", aliases=["wod"])
+    async def world_of_darkness(self, ctx: Context) -> None:
         """
         Commands for playing "World of Darkness".
         """
@@ -26,7 +26,7 @@ class WorldOfDarkness(commands.Cog):
             await ctx.send_help(ctx.command)
 
     @world_of_darkness.command(name="roll")
-    async def roll_wod(self, ctx: commands.Context, dices: int, difficult: int = 6, mod: int = 0):
+    async def wod_roll(self, ctx: Context, dices: int, difficult: int = 6, mod: int = 0) -> None:
         """
         Basic characteristic check for "World of Darkness".
 
@@ -43,63 +43,61 @@ class WorldOfDarkness(commands.Cog):
         If the player has exactly 0 successes, the player fails the check.
         If the player has less than 0 successes, the player fails the test with the indicator "Critical".
         """
-        emb = discord.Embed()
+        embed = Embed()
 
         if dices > 20 or dices <= 0:
-            emb.title = "Please, stop!"
-            emb.description = "Count of dices should be in range 0-20!"
-            await ctx.send(embed=emb)
+            embed.title = "Please, stop!"
+            embed.description = "Count of dices should be in range 0-20!"
+            await ctx.send(embed=embed)
             return
 
         successes = 0
         roll_list = []
 
-        while dices != 0:
+        rolls = dices
+        while rolls != 0:
             dice = random.randint(1, 10)
             if dice == 10:
                 successes += 1
-                dices += 1
+                rolls += 1
             elif dice == 1:
                 successes -= 1
             elif dice >= difficult:
                 successes += 1
-            dices -= 1
+            rolls -= 1
             roll_list.append(dice)
 
         total = successes + mod
 
         if total > 0:
-            emb.set_author(name="SUCCESS")
-            emb.color = RollColors.success
+            embed.set_author(name="SUCCESS")
+            embed.color = RollColors.success
         elif total == 0:
-            emb.set_author(name="FAILURE")
-            emb.color = RollColors.failure
+            embed.set_author(name="FAILURE")
+            embed.color = RollColors.failure
         else:
-            emb.set_author(name="CRITICAL FAILURE")
-            emb.color = RollColors.failure_crit
+            embed.set_author(name="CRITICAL FAILURE")
+            embed.color = RollColors.failure_crit
 
         roll_list_sorted = sorted(roll_list)
 
         roll_info = f"""
         Here you can learn more about your roll.
-
-        ╔══════════════════════════════╗
-        ║ {roll_list} - {len(roll_list)} rolls
-        ║ {roll_list_sorted}
-        ╠══════════════════════════════╣
-        ║ **TARGET**: {difficult}
-        ║ **SUCCESSES**: {successes}
-        ║ **MOD**: {mod}
-        ║ **TOTAL**: {total}
-        ╚══════════════════════════════╝
+        ────────────────────────
+        **Rolls:** `{dices} ({len(roll_list) - dices})`
+        **Regular:** `{", ".join([str(roll) for roll in roll_list])}`
+        **Sorted:** `{", ".join([str(roll) for roll in roll_list_sorted])}`
+        ────────────────────────
+        **Target**: `{difficult}` **Mod**: `{mod}`
+        **Successes**: `{successes}` **Total**: `{total}`
         """
 
-        emb.description = textwrap.dedent(roll_info)
+        embed.description = textwrap.dedent(roll_info)
 
-        await ctx.send(embed=emb)
+        await ctx.send(embed=embed)
 
     @world_of_darkness.command(name="madness")
-    async def roll_madness_wod(self, ctx: commands.Context, dices: int, difficult: int = 6):
+    async def wod_madness(self, ctx: commands.Context, dices: int, difficult: int = 6) -> None:
         """
         A test to determine whether a vampire will fall into madness or not.
 
@@ -114,7 +112,7 @@ class WorldOfDarkness(commands.Cog):
         If the player manages to score 5 successes in a longer or shorter time,
         then the vampire manages to cope with the urges of the Beast.
         """
-        embed = discord.Embed()
+        embed = Embed()
 
         successes = 0
         roll_list = []
@@ -131,30 +129,36 @@ class WorldOfDarkness(commands.Cog):
             dices -= 1
             roll_list.append(dice)
 
-        if successes == 5:
+        if successes >= 5:
             embed.set_author(name="Sobriety of mind has triumphed.")
             embed.description = """
                                 You have managed to suppress the beast inside you.
                                 """
             embed.color = RollColors.success
-            embed.set_footer(text=f"{roll_list} - {successes}")
         elif successes > 0:
             embed.set_author(name="Temporary self-control.")
             embed.description = """
                                 You are temporarily suppressing the beast within you. Don't relax.
                                 """
             embed.color = RollColors.failure
-            embed.set_footer(text=f"{roll_list} - {successes}")
         else:
             embed.set_author(name="The beast takes over.")
             embed.description = """
                                 You could not overcome the beast in yourself. Prepare for the worst.
                                 """
             embed.color = RollColors.failure_crit
-            embed.set_footer(text=f"{roll_list} - {successes}")
+
+        info = f"""
+        **Rolls:** `{", ".join([str(roll) for roll in roll_list])}`
+        **Sorted:** `{", ".join([str(roll) for roll in sorted(roll_list)])}`
+        **Difficult:** `{difficult}` **Successes:** `{successes}`
+        """
+
+        embed.description += textwrap.dedent(info)
 
         await ctx.send(embed=embed)
 
 
-def setup(bot: IncarnBot):
+def setup(bot: IncarnBot) -> None:
+    """Loads WorldOfDarkness Cog."""
     bot.add_cog(WorldOfDarkness())
